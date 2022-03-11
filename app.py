444e2d17829e11d8526c0d636e8557795fc06f1a
hillsar13 <95551195+hillsar13@users.noreply.github.com>
2022-03-10 20:27:14 -0700

    app.py

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
-import sys
-
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
